2483945938c9d6707e82f2c36159608bf51265f5
Sanjeev Vibhakar <sanjeev1913051060@gmail.com>
2025-07-12 21:44:15 +0530

    ✅ Add tabs component and fix imports

--- public/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
-});
