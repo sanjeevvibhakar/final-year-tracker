2e730b8a72041a509f3b9046a35aa1dfac52f24e
Sanjeev Vibhakar <sanjeev1913051060@gmail.com>
2025-07-12 20:36:02 +0530

    Fix: proper React app setup for Vercel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css'; // or comment out if missing
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
